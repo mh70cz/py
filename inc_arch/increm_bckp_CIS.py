18d7d1ef37afcd5554ec690b7f236d977f764418
mh70cz <mh70@mh70.cz>
2020-04-12 12:51:29 +0200

    save_and_backup_logs

--- inc_arch/increm_bckp_CIS.py
+++ inc_arch/increm_bckp_CIS.py
@@ -54,16 +54,14 @@ rootFolders_M = [
 ]
 backup_folder_M = r"C:\Users\m.houska\OneDrive\ZZ-Archive\MDrive_increm"
 
+folder_logs = r"c:\tmp\logs"
 backup_folder_logs = r"C:\Users\m.houska\OneDrive\ZZ-Archive\logs"
-
-now = dt.datetime.now()
-
-error_log_path = r"c:\tmp\logs\error_log" + now.strftime("%Y-%m-%d") +".txt"
-copy_log_path = r"c:\tmp\logs\copy_log" + now.strftime("%Y-%m-%d") +".txt"
 
 ignored_filenames = ["Thumbs.db", "mh_tmp.txt", "mh_wrk.txt"]
 ignored_extension = ["tmp", "wrk", "bak"]
 delta_days = 100
+
+now = dt.datetime.now()
 before = now - dt.timedelta(days=delta_days)
 
 error_log = []
@@ -75,38 +73,49 @@ file_counter = 0
 def haf():
 
     do_bck(rootFolders_oneD, backup_folder_oneD)
-    do_bck(rootFolders_CIS_NB, backup_folder_CIS_NB)
+    # do_bck(rootFolders_CIS_NB, backup_folder_CIS_NB)
     # do_bck(rootFolders_M, backup_folder_M, 16_000_000)
 
     for msg in error_log:
         pass
         print(msg)
 
-    with open(error_log_path, "w") as f:
-        f.write("\n".join(error_log))
-
     copy_log.append(
         "*****************\n"
         f"Total files count / Total files size: {file_counter} / {size_counter}"
-        )
+    )
+
+    save_and_backup_logs(folder_logs, backup_folder_logs)
+
+
+def save_and_backup_logs(folder_logs, backup_folder_logs):
+
+    error_log_path = (
+        folder_logs + "\\" + "error_log_" + now.strftime("%Y-%m-%d") + ".txt"
+    )
+    copy_log_path = folder_logs + "\\" + "copy_log_" + now.strftime("%Y-%m-%d") + ".txt"
+
+    if not os.path.exists(folder_logs):
+        os.makedirs(folder_logs)
+
+    with open(error_log_path, "w") as f:
+        f.write("\n".join(error_log))
+
     with open(copy_log_path, "w") as f:
         f.write("\n".join(copy_log))
-    
 
-    log_paths =[error_log_path, copy_log_path]
+    if not os.path.exists(backup_folder_logs):
+        os.makedirs(backup_folder_logs)
+
+    log_paths = [error_log_path, copy_log_path]
     for path in log_paths:
-
         dst_path = backup_folder_logs + "\\" + os.path.basename(path)
-
-        dst_folder = os.path.dirname(dst_path)
-        if not os.path.exists(dst_folder):
-            os.makedirs(dst_folder)
         try:
             shutil.copy2(path, dst_path)
             pass
         except Exception as e:
             msg = "some problem ({0}) with: {1} ".format(e.args[1], path)
-            print(msg)         
+            print(msg)
 
 
 def do_bck(rootFolders, backup_folder, size_limit=SIZE_LIMIT_DEFAULT):
@@ -115,8 +124,8 @@ def do_bck(rootFolders, backup_folder, size_limit=SIZE_LIMIT_DEFAULT):
     global size_counter, file_counter
     for rootFold in rootFolders:
         for folderName, subfolders, filenames in os.walk(rootFold):
-            at_least_once_passed = False # ie file copied
-            folder_size_counter = 0 
+            at_least_once_passed = False  # ie file copied
+            folder_size_counter = 0
             folder_file_counter = 0
 
             dst_path_prefix = (
@@ -133,7 +142,7 @@ def do_bck(rootFolders, backup_folder, size_limit=SIZE_LIMIT_DEFAULT):
             for subfolder in subfolders:  # pylint: disable=unused-variable
                 # print("Subfolder of " + folderName + ": " + subfolder)
                 pass
-            for filename in filenames:                
+            for filename in filenames:
                 if filename in ignored_filenames:
                     continue
                 extension = os.path.splitext(filename)[-1].lower()
@@ -171,20 +180,20 @@ def do_bck(rootFolders, backup_folder, size_limit=SIZE_LIMIT_DEFAULT):
                     dst_path = dst_path_prefix + path.replace(
                         os.path.dirname(rootFold), ""
                     )
-                    #copy_log.append(dst_path)
+                    # copy_log.append(dst_path)
                     dst_folder = os.path.dirname(dst_path)
                     if not os.path.exists(dst_folder):
                         os.makedirs(dst_folder)
                     try:
-                        shutil.copy2(path, dst_path)
-                        #import time; time.sleep(0.05)
+                        # shutil.copy2(path, dst_path)
+                        # import time; time.sleep(0.05)
                         pass
                     except Exception as e:
                         msg = "some problem ({0}) with: {1} ".format(e.args[1], path)
                         error_log.append(msg)
                         # print(msg)
                     else:
-                        if not at_least_once_passed: # first passed
+                        if not at_least_once_passed:  # first passed
                             at_least_once_passed = True
                             cf_name = "The current folder is " + folderName
                             print(cf_name)
@@ -197,19 +206,17 @@ def do_bck(rootFolders, backup_folder, size_limit=SIZE_LIMIT_DEFAULT):
                             filename + " modi: " + str(mod_time) + " size: " + str(size)
                         )
                         print(
-                            f"Folder files count / Folder files size: " 
+                            f"Folder files count / Folder files size: "
                             f"{folder_file_counter} / {folder_size_counter} \r",
                             end="",
-                            )
+                        )
             if at_least_once_passed:
                 print(
-                    f"Folder files count / Folder files size: " 
+                    f"Folder files count / Folder files size: "
                     f"{folder_file_counter} / {folder_size_counter}",
                     f"  * Total file count / Total files size: "
-                    f"{file_counter} / {size_counter}" 
-                    )
-            
-
+                    f"{file_counter} / {size_counter}",
+                )
 
 
 haf()
