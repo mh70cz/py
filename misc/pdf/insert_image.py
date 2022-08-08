7fc23f2133624c787e1dd4856322d48251cc6f0e
mh70cz <mh70@mh70.cz>
2022-08-09 01:16:19 +0200

    ttfont

--- misc/pdf/insert_image.py
+++ misc/pdf/insert_image.py
@@ -1,5 +1,7 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from reportlab.pdfgen import canvas
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
 import io
 from pathlib import Path
 
@@ -11,10 +13,38 @@ out_pdf_file = src_fld / 'out_with_img.pdf'
 img_file_1 = src_fld / 'Martin_karta_poj_1.png'
 img_file_2 = src_fld / 'Martin_karta_poj_2.png'
 
+name = "Martin Houska"
+address = "Lamačova 824/9, Praha 5, 152 00"
+resp_m = "Irena Housková"
+resp_f = "Miroslav Houska"
+
+tel_m = "605 337 039"
+tel_f = "731 126 295"
+
+email_m = "houskova.irena@gmail.com"
+email_f = "mh70@mh70.cz"
+
+
 def add_image():
     packet = io.BytesIO()
     can = canvas.Canvas(packet)
-    #can.drawString(10, 100, "Hello world")
+
+    pdfmetrics.registerFont(TTFont('Vera', 'Vera.ttf'))
+
+    can.setFont("Vera", 12)
+    can.drawString(120, 766, name)
+    
+    can.drawString(120, 745, address)
+
+    can.drawString(200, 725, resp_m)
+    can.drawString(120, 705, tel_m )
+    can.drawString(300, 705, email_m  )
+
+    can.drawString(200, 685, resp_f)
+    can.drawString(120, 662, tel_f )
+    can.drawString(300, 662, email_f  )
+
+
     x_start_1 = 90
     y_start_1 = -20
     x_start_2 = 310
