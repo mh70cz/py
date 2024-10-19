21a032b89d54858b806dfc5a33f4bfb7874a28a8
mh70cz <mh70@mh70.cz>
2024-10-19 20:58:56 +0200

    .

--- ahk/rnd_vin.py
+++ ahk/rnd_vin.py
@@ -3,6 +3,26 @@ from random import choice
 from pathlib import Path
 
 VINS = [
+    "1N4AL3AP4FC236561",
+    "1FAFP53U23A113292",
+    "2FMDK3JC7DBB66599",
+    "3MZBM1V72EM113930",
+    "1FDKF37G2TEB57107",
+    "4M2EU47E88UJ62010",
+    "1J4NT1FB3BD249992",
+    "5XXGR4A66CG095360"
+]
+
+out_fld = Path("c:/tmp/ahk/")
+out_pth = out_fld / "rnd_vin.txt"
+
+rnd_vin = choice(VINS)
+
+with open(out_pth, "w") as f:
+    f.write(rnd_vin)
+
+
+"""
     "1FAHP35N98W293576",
     "1FTYR14V4XPA45478",
     "KM8SR4HF2FU064035",
@@ -14,12 +34,4 @@ VINS = [
     "1HGFA16559L013534",
     "NMTBA3BE40R046913",
     "NMTBA3BE50R049593"
-]
-
-out_fld = Path("c:/tmp/ahk/")
-out_pth = out_fld / "rnd_vin.txt"
-
-rnd_vin = choice(VINS)
-
-with open(out_pth, "w") as f:
-    f.write(rnd_vin)
+"""
